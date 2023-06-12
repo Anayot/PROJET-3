096a863dc32e43a25ce1cc8b7a0ebee3515472ec
anaelle.yot <anaelle.yot@phidelys.com>
2023-06-12 19:29:18 +0200

    Update projets.js

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,20 +1,24 @@
-fetch("http://localhost:5678/api/works")
-    .then(gallery => gallery.json())
-    .then(projets => {
-        console.log(projets)
-        const images = projets.map(projet => projet.imageUrl);
-        const titres = projets.map(projet => projet.title);
-        const galleryElement = document.querySelector('#gallery');
-        for (let i=0; i < projets.length; i++) {
-            const projetElement = document.createElement('figure');
-            projetElement.innerHTML = `<img src="${images[i]}" alt="${titres[i]}"></img> <figcation>${titres[i]}</figcaption>`
-            galleryElement.appendChild(projetElement)
-        };
+const projets = window.localStorage.getItem("projets");
+if(projets === null){
+    const reponse = await fetch("http://localhost:5678/api/works");
+    projets = await reponse.json();
+    const valeurProjets = JSON.stringify(projets);
+    window.localStorage.setItem("projets", valeurProjets);
+}else{
+    projets = JSON.parse(projets);
+};
 
-        const filtres = document.querySelector('#filtres');
-            console.log(filtres)
+const images = projets.map(projet => projet.imageUrl);
+const titres = projets.map(projet => projet.title);
+const galleryElement = document.querySelector('#gallery');
+for (let i=0; i < projets.length; i++) {
+    const projetElement = document.createElement('figure');
+    projetElement.innerHTML = `<img src="${images[i]}" alt="${titres[i]}"></img> <figcation>${titres[i]}</figcaption>`
+    galleryElement.appendChild(projetElement)
+};
 
-    });
+const filtres = document.querySelector('#filtres');
+        console.log(filtres.categoryId)
 
 
 
