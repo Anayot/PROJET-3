1e7d9873d2f7ad56fec07ccb3a4317180ec6521f
anaelle.yot <anaelle.yot@phidelys.com>
2023-06-14 14:09:32 +0200

    Update projets.js

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,21 +1,60 @@
-fetch("http://localhost:5678/api/works")
-    .then(gallery => gallery.json())
-    .then(projets => {
-        console.log(projets)
-        const images = projets.map(projet => projet.imageUrl);
-        const titres = projets.map(projet => projet.title);
-        const galleryElement = document.querySelector('#gallery');
-        for (let i=0; i < projets.length; i++) {
-            const projetElement = document.createElement('figure');
-            projetElement.innerHTML = `<img src="${images[i]}" alt="${titres[i]}"></img> <figcation>${titres[i]}</figcaption>`
-            galleryElement.appendChild(projetElement)
-        };
+const url = "http://localhost:5678/api/works";
 
-        const filtres = document.querySelector('#filtres');
-            console.log(filtres.categoryI)
+    fetch(url)
+        .then(gallery => gallery.json())
+        .then(projets => {
+            console.log(projets)
+            const images = projets.map(projet => projet.imageUrl);
+            const titres = projets.map(projet => projet.title);
+
+            const galleryElement = document.querySelector('#gallery');
+            for (let i=0; i < projets.length; i++) {
+                const projetElement = document.createElement('figure');
+                projetElement.innerHTML = `<img src="${images[i]}" alt="${titres[i]}"></img> <figcation>${titres[i]}</figcaption>`
+                galleryElement.appendChild(projetElement)
+            };
+
+        })
+
+
+
+
+
+
+        /*
+            // Paramêtre des boutons de filtre
             
+            function filterPorduct(value){
+                const filtres = document.querySelectorAll(".btn-value");
+                filtres.forEach((btn) => {
+                    if(value.toUpperCase() == btn.innerText.toUpperCase()) {
+                        btn.classList.add("active");
+                    }else {
+                        btn.classList.remove("active")
+                    }
+                });
 
-    });
+                //selectionner les projets
+                const categories = projets.map(projet => projet.categoryId);
+                categories.forEach((category) => {
+                    if(value == "tous") {
+                        category.classList.remove("hide");
+                    }else {
+                        if(category.classList.contains(value)) {
+                            category.classList.remove("hide");
+                        }else {
+                            category.classList.add("hide");
+                        }
+                    }
+                })
+            }
+
+
+            window.onload = () => {
+                filterPorduct("tous");
+            };
+
+        
 
 
 
